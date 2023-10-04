66ee7c1bbcab1dd345d6fcab630730105a6aeb13
cm1998Work <146945011+cm1998Work@users.noreply.github.com>
2023-10-04 14:14:59 +0100

    Update weather_api.py

--- scripts/weather_api.py
+++ scripts/weather_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
+from database import openDB, writeToDB, closeDB
 Key = "a56cdd37810f0a936a6db3bcadd4188c"
-
 
 
 def callWeatherAPI(latitude: tuple, longitude: tuple, key: str) -> dict:
@@ -12,17 +12,33 @@ def callWeatherAPI(latitude: tuple, longitude: tuple, key: str) -> dict:
   """
   
   Base_url = "https://api.openweathermap.org/data/3.0/onecall?"
-  request_url = f"{Base_url}lat={latitude}&lon={longitude}&appid={key}"
+  request_url = f"{Base_url}lat={latitude}&lon={longitude}&appid={key}&units=metric"
   response = requests.get(request_url)
   data = json.dumps(response.text)
   
   return data
+
+def TransformResponseData(data: dict)->dict:
+  """
+  Tranforms the API response so that it is ready to be 
+  written to the database
+  """
+  current = data.current
+  del current['weather'] #Weather attribute is an object (can't be visualised so delete)
+  #do more 
+  return current
+  
+  
+
+  
   
 
 
 if __name__ == "__main__":
 
-  callWeatherAPI((90,90),(180,-180), Key)
+  data = callWeatherAPI((90,90),(180,-180), Key)
+  data = transformResponseData(data)
+  writeToDB(data)
 
 
   
