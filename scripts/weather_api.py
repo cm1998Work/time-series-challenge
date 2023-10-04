2c64c1e9588b333d442e60532785444ee40d6e3e
cm1998Work <146945011+cm1998Work@users.noreply.github.com>
2023-10-04 16:42:02 +0100

    Update weather_api.py

--- scripts/weather_api.py
+++ scripts/weather_api.py
@@ -1,60 +1,95 @@
 import requests
 import json
 import time
-from influxdb_client_3 import Point
+from influxdb_client_3 import InfluxDBClient3, Point
+
 
 Key = "a56cdd37810f0a936a6db3bcadd4188c"
 
 
 def callWeatherAPI(latitude: float, longitude: float, key: str) -> dict:
-   
-  """
-  Builds an API request for the OpenWeather API based on the parameters 
-  passed to the function. 
-  Response is parsed as a dict and returned.
-  """
-  
-  Base_url = "https://api.openweathermap.org/data/2.5/weather?"
-  request_url = f"{Base_url}lat={latitude}&lon={longitude}&appid={key}&units=metric"
-  response = requests.get(request_url)
-  data = json.loads(response.text)
-  
-  return data
+
+    Base_url = "https://api.openweathermap.org/data/2.5/weather?"
+    request_url = f"{Base_url}lat={latitude}&lon={longitude}&appid={key}&units=metric"
+    response = requests.get(request_url)
+    data = json.loads(response.text)
+
+    return data
 
 def transformResponseData(data: dict)->dict:
+
+    #Access weather data - we will use temp and wind
+    temp = data['main']
+    wind = data['wind']
+    time = data['dt']
+
+
+    #Create data structure which can be written to influx DB
+    transformed_data = {
+        'measurement':"Weather",
+        'tags':{'location':'London'},
+        'fields':{},
+    }
+    for key, value in temp.items():
+        transformed_data['fields'][key] = value
+    for key, value in wind.items():
+        transformed_data['fields'][key] = value
+
+
+    print(transformed_data)
+    return transformed_data
+
+
+
+
+
+def openDB():
   """
-  Tranforms the API response so that it is ready to be 
-  written to the database
-  Inflix DB will accept dictionary style mapping with keys:
-  measurement, tags, fields and time
+  Opens a connection to the database and returns
+  the database object
   """
   
-  #Access weather data - we will use temp and wind
-  temp = data['main']
-  wind = data['wind']
-  time = data['dt']
+  host = "https://us-east-1-1.aws.cloud2.influxdata.com"
+  token = "Toh-HGqnerXAdabwVlJFWlLUA1nlGlZVQypF8SB9HA6hVeBJTleaAAHz8ymTvXT7JRioe7e5g1FErjVRGHTWfA=="
+  org = "Data"
   
-  #Create data structure which can be written to influx DB
-  transformed_data = {
-    'measurement':"Weather",
-    'tags':{'location':'London'},
-    'fields':{},
-    'time':time
-  }
-  for key, value in temp.items():
-    transformed_data['fields'][key] = value
-  for key, value in wind.items():
-    transformed_data['fields'][key] = value
+
+  client = InfluxDBClient3(
+    host=host,
+    token=token,
+    org=org
+  )
+  
+  return client
   
   
-  print(transformed_data)
-  return transformed_data
+  return db
+
+
+def writeToDB(data: dict):
+  """
+  Writes the data to the database
+  """
+  db = openDB()
+  print(db)
+  database = "challenge"
+
+  #write_api = db.write_api(write_options=SYNCHRONOUS)
+  db.write(database=database, record=data)
+
+  closeDB(db)
+  
+
+
+def closeDB(db):
+  """
+  Closes the connection to the database
+  """
+  db.close()
+
 
 if __name__ == "__main__":
 
-  data = callWeatherAPI(51.5072, -0.118092, Key) #Latitude and longitude for London passed to function
-  transformed_data = transformResponseData(data)
-  writeToDB(transformed_data)
-
-
-  
+    data = callWeatherAPI(51.5072, -0.118092, Key) #Latitude and longitude for London passed to function
+    transformed_data = transformResponseData(data)
+    writeToDB(transformed_data)
