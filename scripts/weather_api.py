2e656369475dbaa0c92cd413468e62befa991b4e
cm1998Work <146945011+cm1998Work@users.noreply.github.com>
2023-10-04 14:59:57 +0100

    Update weather_api.py

--- scripts/weather_api.py
+++ scripts/weather_api.py
@@ -4,7 +4,7 @@ from database import openDB, writeToDB, closeDB
 Key = "a56cdd37810f0a936a6db3bcadd4188c"
 
 
-def callWeatherAPI(latitude: tuple, longitude: tuple, key: str) -> dict:
+def callWeatherAPI(latitude: float, longitude: float, key: str) -> dict:
   """
   Builds an API request for the OpenWeather API based on the parameters 
   passed to the function. 
@@ -22,10 +22,28 @@ def TransformResponseData(data: dict)->dict:
   """
   Tranforms the API response so that it is ready to be 
   written to the database
+  Inflix DB will accept dictionary style mapping with keys:
+    measurement, tags, fields and time
   """
+  transformed_data = []
   current = data.current
-  del current['weather'] #Weather attribute is an object (can't be visualised so delete)
-  #do more 
+  del current['weather'] #Weather attribute value is a dictionary - remove
+
+  measurement = "Weather"
+  tag = {'location':'London')
+  for key, value in current.items()
+    
+    
+    
+    obj = {}
+    
+
+    
+
+  
+
+  
+  
   return current
   
   
@@ -36,7 +54,7 @@ def TransformResponseData(data: dict)->dict:
 
 if __name__ == "__main__":
 
-  data = callWeatherAPI((90,90),(180,-180), Key)
+  data = callWeatherAPI(51.5072, -0.118092, Key) #Latitude and longitude for London passed to function
   data = transformResponseData(data)
   writeToDB(data)
 
