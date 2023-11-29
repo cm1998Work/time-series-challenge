1927b3ef5769aebdbb0662e619b350321fc22bc2
cm1998Work <146945011+cm1998Work@users.noreply.github.com>
2023-11-29 11:29:02 +0000

    Update more_analysis.py

--- scripts/more_analysis.py
+++ scripts/more_analysis.py
@@ -1,1 +1,17 @@
-from weather_api import open_db, close_db
+from weather_api import openDb, closeDb
+import pandas as pd
+
+def getAllData(db)->pd.DataFrame:
+  query = """SELECT  *
+  FROM "Weather"
+  """
+  
+  data = db.query(
+    query = query,
+    database = "challenge",
+    language = "sql"
+  )
+
+df = table.to_pandas().sort_values(by="time")
+print(df)
+return df
